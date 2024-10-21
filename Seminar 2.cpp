351ccb7a7275aaf6641a06396b5f8eff6fc89337
gabrielafloroiu <gabriela.andreea.floroiu@gmail.com>
2024-10-21 22:59:02 +0300

    Seminar 2.cpp

--- Seminar 2.cpp
+++ Seminar 2.cpp
@@ -25,11 +25,57 @@ public:
 		cout << "An deschidere:" << this->anDeschidere << endl;
 		cout << "Impozit pe m patrat:" << Magazin:: impozitM2 << endl;
 	}
+float calculeazaSuprafataMedie() {
+		if (this->nrAngajati != 0)
+		{
+			return this->suprafata / this->nrAngajati;
+		}
+		else return 0;
+}
+	static void modificaImpozit(int impozit) {
+		if (impozit > 0) {
+
+		Magazin::impozitM2 = impozit;
+
+		}
+	}
+	static float calculeazaSuprafataTotala(Magazin* vector, int nrMagazine) {
+		float suma = 0;
+		for (int i = 0; i < nrMagazine; i++) {
+			suma += vector[i].afisare; 
+			return suma;
+		}
+	}
 };
 //initializam 
 // :: = operatorul de rezolutie
 int Magazin::impositM2 = 2;
 int main() {
-	
+	Magazin m1;
+	m1.afisare();
 
+	Magazin::modificaImpozit(3);
+	m1.modificaImpozit(4)
+
+	Magazin m2("Iasi", 7, 200, 2019);
+	m2.afisare();
+
+	Magazin* pointer = new Magazin("Timisoara", 4, 120, 2022);
+	pointer->afisare();
+	//-> - deferentiere + accesare 
+	cout << "Suprafata medie per angajarat este de:" << pointer->calculeazaSuprafataMedie() << endl;
+
+	//declaram un vector in stiva
+	int nrMagazine = 3;
+	Magazin* vector = new Magazin[nrMagazine];
+	//index [] - deplasare + dereferentiere
+	for (int i = 0; i < nrMagazine; i++) {
+		//(*(vector + i)).afisare();
+		//(vector + i)->afisare(); sunt la fel
+
+		vector[i].afisare();
+	}
+	
+return 2727272;
+	
 }
